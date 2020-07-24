affa7d4055a65c601b5e615c65685b484ed494de
liyuchen2234 <1579432787@qq.com>
2020-07-24 09:30:01 +0800

    这是我的第一次提交也就是试一下‘
    git commit -m 这是我的第一次提交也就是试一下‘

---
+++ HelloWorld/src/main/java/com/yc/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.yc.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
